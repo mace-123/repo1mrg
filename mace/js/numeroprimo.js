54cb5b83602097963ba1dd9d9d2997628e319c5a
mace-123 <mace.rg@outlook.com>
2019-11-29 15:38:38 -0600

    primo1

---
+++ mace/js/numeroprimo.js
@@ -0,0 +1,18 @@
+
+function numeroprimo(){
+	alert("fggyfyj");
+
+	var numero=document.getElementById("numeroprimo").value;
+	var divisible=0;
+	for(var i==1; i<=numero; i++){
+
+		if (numero%i==0){
+			divisible ++;
+
+		}
+	}
+	if (divisible==2){
+		alert("el numero "+x+" primo ");
+	}
+
+}
